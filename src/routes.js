42e62f41adc425960384e49134bc9e9787c4a456
Mamedes <mamedeser@gmail.com>
2020-07-01 08:10:42 -0400

    first Commit - Modelo Base

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleViseble: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+      },
+    },
+  ),
+);
+
+export default Routes;
